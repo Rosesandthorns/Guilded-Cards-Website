916cb9732546d84060997ee01a6614bd3a79914a
Rosesandthorns <powwerofpowwer@gmail.com>
2025-02-27 10:29:58 -0500

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,14 +1,14 @@
 // profile.js (Profile-specific logic)
 
 // Function to update the profile page content
-function updateProfilePage(user) {
+export function updateProfilePage(user) { // ADD the 'export' keyword here
     const profileContentDiv = document.getElementById("profileContent");
     if (profileContentDiv) {
         if (user) {
             // User is signed in, show profile details
             profileContentDiv.innerHTML = `
-                <h1 style="text-align: center;"><span class="math-inline">\{user\.displayName\}'s Profile</h1\>
-<p\></span>{user.displayName}'s Deck</p>`;
+                <h1 style="text-align: center;">${user.displayName}'s Profile</h1>
+                <p>${user.displayName}'s Deck</p>`;
         } else {
             // User is not signed in, show sign-in message
             profileContentDiv.innerHTML = `<p>Please sign in to view profile</p>`;
